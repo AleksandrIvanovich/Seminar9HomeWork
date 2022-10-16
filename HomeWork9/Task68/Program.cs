428daa6089aee599a52ea63b6a35933a7444de85
Aleksandr <fomichov73@mail.ru>
2022-10-16 14:37:18 +0300

    Added solution task 68

---
+++ HomeWork9/Task68/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 68: Напишите программу вычисления
+ функции Аккермана с помощью рекурсии.
+*/
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+
+int n = GetNumber("Введите первое положителькое число:");
+int m = GetNumber("Введите второе положительное число:");
+
+int FunkAkkerman(int n, int m)
+{
+    if (n == 0)
+     return m + 1;
+    if (n != 0 && m == 0)
+     return FunkAkkerman(n - 1, 1);
+    if (n > 0 && m > 0)
+     return FunkAkkerman(n - 1, FunkAkkerman(n, m - 1));
+    return FunkAkkerman(n,m);
+}
+Console.WriteLine($"A({n},{m}) = {FunkAkkerman(n,m)}");
